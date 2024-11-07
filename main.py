da02168b25e2584654a068f46f8dd13e0b7451ee
amin-tennah <amin.tennah@edu.esiee.fr>
2024-11-07 10:49:25 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from math import sqrt
-
+"""fonction calcule"""
 #### Fonction secondaire
 
 
 def isprime(p):
-
+    """fonction calcule"""
     # votre code ici
-
-    pass
+    if p in (0,1):
+        return False
+    for d in range(2, int(p**0.581) + 1):
+        if p % d == 0:
+            return False
+    return True
 
 #### Fonction principale
-
-
 def main():
-
+    """fonction calcule"""
     # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
